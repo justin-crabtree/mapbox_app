1baca9535db43c2df5d3a554e7b791fd729ee7b2
crabtree <justin.kyle.crabtree@gmail.com>
2019-10-17 15:31:26 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div id="map"></div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+<style>
+body {
+  margin: 0;
+  padding: 0;
 }
+#map {
+  top: 0;
+  bottom: 0;
+  height: 700px;
+}
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Venice, FL!",
+      places: [
+        {
+          longitude: -82.39323,
+          latitude: 27.150783,
+          description: "Home"
+        },
+        {
+          longitude: -82.446086,
+          latitude: 27.099614,
+          description: "Upper Crust Cafe and Bakery"
+        },
+        {
+          longitude: -82.460151,
+          latitude: 27.099909,
+          description: "Venice Beach"
+        }
+      ]
+    };
+  },
+  created: function() {},
+  mounted: function() {
+    mapboxgl.accessToken = "pk.eyJ1IjoiZHphZ2hpYW4iLCJhIjoiY2pzbnF0NmV0MGY2czQzbXBpMjcwMzRmNiJ9.Jei4-17Vu7hJSerisjPCEg";
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-82.4542633, 27.0997775], // starting position [lng, lat]
+      zoom: 12 // starting zoom
+    });
+
+    this.places.forEach(function(place) {
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      new mapboxgl.Marker()
+        .setLngLat([place.longitude, place.latitude])
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(map);
+    });
+  },
+  methods: {}
+};
 </script>
